d249a11a3fc8ea0df9eeb386ef570706b7ae86bf
kyle webb <kylewebb4@weber.edu>
2017-02-28 17:18:49 +0000

    first commit

---
+++ Juventus_hw6_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Juventus_hw6_task1.c
+ *       	Usage:  ./Juventus_hw6_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:18:00 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Kyle Webb (), kylewebbear@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* Standard Library */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
